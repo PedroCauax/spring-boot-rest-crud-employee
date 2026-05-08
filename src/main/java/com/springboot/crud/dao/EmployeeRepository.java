c30b643c55e03eeeb81621245f84e7a5f3e251b2
Pedro Cauã <pcaua6372@gmail.com>
2026-05-08 10:45:33 -0300

    Config Data Rest

--- src/main/java/com/springboot/crud/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/crud/dao/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package com.springboot.crud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 
 import com.springboot.crud.entity.Employee;
 
+
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     
